eb90fcdc4694b888a0c1909dbe847d09b24db468
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-01-30 15:15:14 -0600

    cambios

--- src/app/componentes/usuario/usuario.component.ts
+++ src/app/componentes/usuario/usuario.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LugaresComponent } from '../lugares/lugares.component';
+import { reservationComponent } from '../reservation/reservation.component';
 
 @Component({
   selector: 'app-usuario',
-  imports: [RouterModule, LugaresComponent],
+  imports: [RouterModule, LugaresComponent, reservationComponent],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css',
 })
