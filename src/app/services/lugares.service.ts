496fab2f1b84e62b1a05aed64c348039b0723951
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-01-30 12:49:22 -0600

    12:47

--- src/app/services/lugares.service.ts
+++ src/app/services/lugares.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Lugares } from './lugares';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LugaresService {
+  constructor() {}
 
-  constructor() { }
+  url = '';
+
+  async getAllLugares(): Promise<Lugares[]> {
+    const data = await fetch(this.url);
+    return await data.json();
+  }
+
+  async getLugaresById(id: number): Promise<Lugares | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return (await data.json()) ?? {};
+  }
 }
