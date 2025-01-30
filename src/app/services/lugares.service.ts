3e0b8c1506c7f9480611122bd2a8d51e54f30d9d
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-01-30 13:18:36 -0600

    nuevos cambios

--- src/app/services/lugares.service.ts
+++ src/app/services/lugares.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Lugares } from './lugares';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LugaresService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  url = '';
+  url = 'http://192.168.1.111/api/listLugaresTuristicos.php';
 
   async getAllLugares(): Promise<Lugares[]> {
     const data = await fetch(this.url);
