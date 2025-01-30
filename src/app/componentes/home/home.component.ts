41fd727005e54f2fbe75af2e08c0eea3a44904d0
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-01-30 12:01:46 -0600

    uwu

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { LugaresComponent } from '../lugares/lugares.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, RouterOutlet],
+  imports: [RouterModule, RouterOutlet, LugaresComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
