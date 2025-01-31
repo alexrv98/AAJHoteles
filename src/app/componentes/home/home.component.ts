519c1fad16677ee6458dfeb1b907dad44c47e6de
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-01-31 09:37:18 -0600

    filtro de busqueda

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,18 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { LugaresService } from '../../services/lugares.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule], // Agrega FormsModule para usar ngModel
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  constructor(private authService: AuthService) {} // Inyecta el servicio
+  lugares: any[] = [];
+  lugaresFiltrados: any[] = [];
+  textoBusqueda: string = ''; // Almacena el texto del input
+  lugaresService: LugaresService = inject(LugaresService);
+
+  constructor(private authService: AuthService) {
+    this.lugaresService.obtenerLugares().subscribe((data: any[]) => {
+      this.lugares = data;
+      this.lugaresFiltrados = data;
+    });
+  }
 
   logout(): void {
-    this.authService.logout(); // Llama la función logout
-    window.location.reload(); // Recargar para reflejar cambios en la UI
+    this.authService.logout();
+    window.location.reload();
+  }
+
+  buscar(event: Event): void {
+    event.preventDefault(); // Evita que el formulario recargue la página
+
+    if (!this.textoBusqueda) {
+      this.lugaresFiltrados = this.lugares;
+      return;
+    }
+
+    this.lugaresFiltrados = this.lugares.filter((lugar) =>
+      lugar.nombre.toLowerCase().includes(this.textoBusqueda.toLowerCase())
+    );
   }
 }
