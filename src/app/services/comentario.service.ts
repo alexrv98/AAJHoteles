508428ad4aaa3e86bf871153cf2f092fa95be959
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-01-31 12:58:32 -0600

    ip

--- src/app/services/comentario.service.ts
+++ src/app/services/comentario.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ComentarioService {
-  private apiUrl = 'http://localhost/api';
+  private apiUrl = 'http://192.168.1.111/api';
 
   constructor(private http: HttpClient) {}
 
@@ -25,18 +25,28 @@ export class ComentarioService {
   }
 
   getHotelesPorLugar(lugar_id: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/listHoteles.php?lugar_id=${lugar_id}`, {
-      headers: this.crearHeaders(),
-    });
+    return this.http.get(
+      `${this.apiUrl}/listHoteles.php?lugar_id=${lugar_id}`,
+      {
+        headers: this.crearHeaders(),
+      }
+    );
   }
 
   getComentarios(hotel_id: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/comentarios.php?hotel_id=${hotel_id}`, {
-      headers: this.crearHeaders(),
-    });
+    return this.http.get(
+      `${this.apiUrl}/comentarios.php?hotel_id=${hotel_id}`,
+      {
+        headers: this.crearHeaders(),
+      }
+    );
   }
 
-  agregarComentario(hotel_id: number, calificacion: number, comentario: string): Observable<any> {
+  agregarComentario(
+    hotel_id: number,
+    calificacion: number,
+    comentario: string
+  ): Observable<any> {
     const data = { hotel_id, calificacion, comentario };
     return this.http.post(`${this.apiUrl}/comentarios.php`, data, {
       headers: this.crearHeaders(),
